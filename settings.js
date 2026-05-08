3e91bdb64a18c1a18110a737ddd2591cadbd4f96
MichiMiau_u1 <ffandedream@gmail.com>
2026-05-07 19:45:09 -0500

    Se añadio settings.js
    Habia olvidado añadirlo, y daba error al iniciar por obvias razonez

---
+++ settings.js
@@ -0,0 +1,39 @@
+import fs from 'fs';
+
+global.owner = ['50376271661', '51982810730', '5351512906']
+global.botNumber = ''
+//Añade el número de telefono donde estara el bot ahi, para recibir code al iniciar, si lo dejas vacio no hay problema, el cambio es que te pedira ingresar el número de telefono en consola manualmente.
+
+global.sessionName = 'Sessions/Owner'
+global.version = '^2.0 - Latest'
+global.dev = "© ⍴᥆ᥕᥱrᥱძ ᑲᥡ .🎀   ֹ   ۪ 𝕸𝕚𝕔𝕙𝕚𝕄𝕚́𝗮𝘂_𝘂𝟭."
+global.wm = "Michi"
+global.links = {
+  api: 'https://api.stellarwa.xyz',
+  channel: "https://whatsapp.com/channel/0029VbCi1wOGE56eEodRVt3K",
+  github: "https://github.com/MichiMiauu1-DEV",
+  gmail: "ffandedream@gmail.com"
+}
+global.my = {
+  ch: '120363409104008533@newsletter',
+  name: '𓆩╚𓊈⃟⃞⇱Michiwii Oficial Channel⇲,⃟⃞𓊉╝𓆪',
+}
+
+global.mess = {
+  socket: '《✧》 Este comando solo puede ser ejecutado por un Socket.',
+  admin: '《✧》 Este comando solo puede ser ejecutado por los Administradores del Grupo.',
+  botAdmin: '《✧》 Este comando solo puede ser ejecutado si el Socket es Administrador del Grupo.'
+}
+
+global.APIs = {
+  adonix: { url: "https://api-adonix.ultraplus.click", key: "Destroy" },
+  vreden: { url: "https://api.vreden.web.id", key: null },
+  nekolabs: { url: "https://api.nekolabs.web.id", key: null },
+  siputzx: { url: "https://api.siputzx.my.id", key: null },
+  delirius: { url: "https://api.delirius.store", key: null },
+  ootaizumi: { url: "https://api.ootaizumi.web.id", key: null },
+  stellar: { url: "https://api.stellarwa.xyz", key: wm },
+  apifaa: { url: "https://api-faa.my.id", key: null },
+  xyro: { url: "https://api.xyro.site", key: null },
+  yupra: { url: "https://api.yupra.my.id", key: null }
+}
