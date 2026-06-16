4fe014671f58edcc59dd84b54fe86bfa72f80ed4
chrisiya-afk <zhyrogaming2005@gmail.com>
2026-06-16 13:36:34 -0400

    Update exec.js

--- commands/owner/exec.js
+++ commands/owner/exec.js
@@ -1,44 +1,54 @@
-import syntaxerror from 'syntax-error'
-import { format } from 'util'
-import { fileURLToPath } from 'url'
-import { dirname } from 'path'
-import { createRequire } from 'module'
+import syntaxerror from 'syntax-error';
+import { format } from 'util';
+import { createRequire } from 'module';
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
-const require = createRequire(__dirname)
 export default {
   command: ['ex', 'e'],
   category: 'owner',
+  description: 'Ejecutar código JavaScript en el bot.',
   isOwner: true,
-  run: async (client, m, args, usedPrefix, command, text) => {
-  if (!text.trim()) {
-   return client.reply(m.chat, '《✧》 Debes escribir un comando a ejecutar.', m)
-  }
-    let _text = (command === 'e' ? 'return ' : '') + text
-    let old = m.exp * 1
-    let _return, _syntax = ''
+  run: async ({ msg, sock, args, command, text, __dirname }) => {
+    const require = createRequire(__dirname);
+    if (!text.trim()) {
+      return sock.reply(msg.chat, '《✧》 Debes escribir un comando a ejecutar.', msg);
+    }
+    
+    // Limpia comillas tipográficas que pone WhatsApp: ‘ ’ “ ”
+    let code = text.replace(/[‘’]/g, "'").replace(/[“”]/g, '"');
+    
+    // Envuelve en IIFE para poder usar await sin poner return
+    let _text = `(async () => { ${code} })()`;
+    let _return, _syntax = '';    
+
     try {
-      await m.react('🕒')
-      let i = 15
-      let f = { exports: {} }
-      let exec = new (async () => {}).constructor('print', 'm', 'client', 'require', 'Array', 'process', 'args', 'module', 'exports', 'argument', _text)
-       _return = await exec.call(client, (...args) => {
-        if (--i < 1) return
-        return client.reply(m.chat, format(...args), m)
-      }, m, client, require, Array, process, args, f, f.exports, [client])
-      await m.react('✔️')
+      await msg.react('🕒');
+      let i = 15;
+      let f = { exports: {} };
+      let exec = new (async () => {}).constructor(
+        'print', 'msg', 'sock', 'require', 'Array', 'process', 'args', 'module', 'exports', 'argument', _text
+      );
+      
+      _return = await exec.call(
+        sock, 
+        (...args) => {
+          if (--i < 1) return;
+          return sock.reply(msg.chat, format(...args), msg);
+        }, 
+        msg, sock, require, Array, process, args, f, f.exports, 
+      );
+      
+      await msg.react('✔️');
     } catch (e) {
-      let err = syntaxerror(_text, 'Execution Function', {
-        allowReturnOutsideFunction: true,
-        allowAwaitOutsideFunction: true,
-        sourceType: 'module'
-      })
-      if (err) _syntax = '```' + err + '```\n\n'
-      _return = e
-      await m.react('✖️')
+      let err = syntaxerror(code, 'Execution Function', { 
+        allowReturnOutsideFunction: true, 
+        allowAwaitOutsideFunction: true, 
+        sourceType: 'module' 
+      });
+      if (err) _syntax = '```' + err + '```\n\n';
+      _return = e;
+      await msg.react('✖️');
     } finally {
-      client.reply(m.chat, _syntax + format(_return), m)
-      m.exp = old
+      sock.reply(msg.chat, _syntax + format(_return), msg);
     }
   }
-}
+};
