1865399fc659fb68fd1d52ba15425b13c5bba3c3
chrisiya-afk <zhyrogaming2005@gmail.com>
2026-07-06 10:32:19 -0400

    Update jeje.js

--- commands/special/jeje.js
+++ commands/special/jeje.js
@@ -4,11 +4,9 @@ export default {
   isOwner: false,
   run: async (client, m, args, usedPrefix, command, text) => {
     try {
-      const audioPath = './commands/14.opus'; // Asegúrate de que la ruta sea correcta
-      await client.sendMessage(m.chat, { audio: { url: audioPath }, mimetype: 'audio/mp4' });
-      m.reply('Michi Accedí a tu GitHub te voy a hackear muejeje');
+      await m.reply('Michi Accedí a tu GitHub te voy a hackear muejeje');
     } catch (e) {
-      m.reply(`😕 Error: ${e.message}`);
+      await m.reply(`😕 Error: ${e.message}`);
     }
   },
 };
